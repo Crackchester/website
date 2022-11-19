0fca9b26081a491f83ffbb028da9feed88836445
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-11-19 02:12:21 +0000

    Created react basis

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="App-nav">
+        <ul>
+          <li><a href="./">Home</a></li>
+          <li><a href="workshops">Workshops</a></li>
+          <li><a href="events">Events</a></li>
+          <li><a href="join">Join Us</a></li>
+          <li><a href="partners">Partners</a></li>
+        </ul>
+      </nav>
+      <header className="App-header">
+        <h1>Crackchester</h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
--- website/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import HomeView
-
-app_name = 'home'
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-]
