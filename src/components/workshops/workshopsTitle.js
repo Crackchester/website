3265c3d00913514521432ba431c6eddb99e6db2d
Georgegoodey <george@goodey.co.uk>
2023-08-04 17:24:56 +0100

    Workshops pull from db

--- src/components/workshops/workshopsTitle.js
+++ src/components/workshops/workshopsTitle.js
@@ -1,27 +1,57 @@
+import React from 'react';
 import WorkshopsList from './workshopsList';
-import workshops from './workshops.json'
 import './Workshops.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperclip } from '@fortawesome/free-solid-svg-icons'
 
-const workshopsTitle = () => {
-  return (
-    <div className='workshops'>
-      {
-        workshops.map((data, index) => {
-          return <>
-            <div className="workshop_title">
-              <h1><FontAwesomeIcon icon={faPaperclip} /> {data.name}</h1>
-            </div>
-            <WorkshopsList 
-              key={index}
-              content={data.content}
-            />
-          </>
-        })
-      }
-    </div>
-  )
+class workshopsTitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workshops: [],
+      isLoading: true
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({isLoading: true});
+
+    const response = await fetch('https://ec2.goodey.co.uk:8443/Workshops', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const data = await response.json();
+    this.setState({workshops: data, isLoading: false});
+  }
+
+  render() {
+    const {workshops, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className='workshops'>
+        {
+          workshops.map((data, index) => {
+            return <>
+              <div className="workshop_title">
+                <h1><FontAwesomeIcon icon={faPaperclip} /> {data.name}</h1>
+              </div>
+              <WorkshopsList 
+                key={index}
+                content={data.content}
+              />
+            </>
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default workshopsTitle;
