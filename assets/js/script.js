01d15b2ef64679efc6f22f54f136b161468d7e92
albert666h <albert.harutyunyan@student.manchester.ac.uk>
2025-02-18 20:25:28 +0000

    fixed some UI and updated the (c) date to 2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,8 @@ particlesJS('particles-js', {
 });
 
 
+const hamburger = document.getElementById('hamburger');
+const navbarRight = document.querySelector('.navbar-right');
+hamburger.addEventListener('click', () => {
+    navbarRight.classList.toggle('active');
+});
